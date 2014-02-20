75f4b4d631c4eb04b822b25a44605d628f7d331b
Pato <plankenau@gmail.com>
2014-02-20 16:34:35 -0600

    added visual toggle

--- hct/hough.py
+++ hct/hough.py
@@ -10,6 +10,8 @@ else:
     imageFile = "balls.jpg"
     parameter = 90
 
+visual = True
+
 
 img = cv2.imread(imageFile,0)
 img = cv2.medianBlur(img,5)
@@ -18,13 +20,14 @@ cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
 circles = cv2.HoughCircles(img,cv2.cv.CV_HOUGH_GRADIENT,1,20,
                             param1=50,param2=parameter,minRadius=0,maxRadius=0)
 
-circles = np.uint16(np.around(circles))
-for i in circles[0,:]:
-    # draw the outer circle
-    cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
-    # draw the center of the circle
-    cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+if visual:
+    circles = np.uint16(np.around(circles))
+    for i in circles[0,:]:
+        # draw the outer circle
+        cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+        # draw the center of the circle
+        cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
 
-cv2.imshow('detected circles',cimg)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    cv2.imshow('detected circles',cimg)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
