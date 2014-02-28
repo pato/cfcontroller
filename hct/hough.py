1340fecb987e7877b509224a8331aef51f9d7fe7
Jonathan <jdc@cs.utexas.edu>
2014-02-28 13:07:02 -0600

    fixed detecting of balls

--- hct/hough.py
+++ hct/hough.py
@@ -8,17 +8,18 @@ if (len(sys.argv) == 3):
     parameter = float(sys.argv[2])
 else:
     imageFile = "balls.jpg"
-    parameter = 90
+    parameter = 72
 
 visual = True
 
 
 img = cv2.imread(imageFile,0)
-img = cv2.medianBlur(img,5)
+#img = cv2.medianBlur(img,3) #medBlur(5) was probably too strong
+img = cv2.blur(img, (3, 3))  #meanBlur is closer to what we want
 cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
 
-circles = cv2.HoughCircles(img,cv2.cv.CV_HOUGH_GRADIENT,1,20,
-                            param1=50,param2=parameter,minRadius=0,maxRadius=0)
+circles = cv2.HoughCircles(img,cv2.cv.CV_HOUGH_GRADIENT,1,60,
+                            param1=40,param2=parameter,minRadius=80,maxRadius=0)
 
 if visual:
     circles = np.uint16(np.around(circles))
