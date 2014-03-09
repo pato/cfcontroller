aa37108e3b475fa4f182422570c4137e01a438c3
Jonathan <jdc@cs.utexas.edu>
2014-03-08 20:17:56 -0600

    Added downsizing image for speed

--- hct/ourGrayHough.py
+++ hct/ourGrayHough.py
@@ -1,4 +1,5 @@
 '''
+Shrink the image to speed up processing while preserving accuracy
 Impelement a non-standard way of converting to grayscale based off of the target color of the ball
 Perform HCT on the new grayscale image
 '''
@@ -8,7 +9,7 @@ import cv2
 import cv2.cv
 import numpy as np
 import math
-
+import time
 
 if (len(sys.argv) == 2):
     imageFile = "images/magenta" + str(sys.argv[1]) + ".png"
@@ -18,37 +19,41 @@ else:
 
 
 visual = True
+start = time.time()
 
 src = cv2.imread(imageFile, cv2.CV_LOAD_IMAGE_COLOR)
-img2 = cv2.GaussianBlur(src, (15, 15), 1)
 
 #constants for the 'perfect' ball color
 pRED = 200
 pGREEN = 1
 pBLUE = 120
 
-#img2 = cv2.resize(img2, (160, 120))
+scale = 2
+img2 = cv2.resize(src, (640/scale, 480/scale))
+img2 = cv2.GaussianBlur(img2, (9, 9), 1)
 
 #convert to gray
 imgGray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY) #REALLY bad, but idk how to create a 2-d array with just a width and height
+
 height, width = imgGray.shape
 
 for i in range(height):
     for j in range(width):
-        
         b = img2.item(i, j, 0)
         g = img2.item(i, j, 1)
         r = img2.item(i, j, 2)
         
         distance = ((b-pBLUE)**2 + (g-pGREEN)**2 + (r-pRED)**2)/3
-        distance = int(math.sqrt(distance))
+        distance = int(math.sqrt(distance)) #can shave off 0.03 seconds on 320x240 image without using sqrt
         if (distance > 70):
             distance = 255
         imgGray.itemset((i, j), distance)
 
 
-circles = cv2.HoughCircles(imgGray,cv2.cv.CV_HOUGH_GRADIENT,4,200,
-                           param1=30,param2=200,minRadius=0,maxRadius=150)
+print("It took " + str(time.time()-start) + " seconds.")
+circles = cv2.HoughCircles(imgGray, cv2.cv.CV_HOUGH_GRADIENT, 4*scale, 200/scale,
+                           param1=30,param2=200,minRadius=0/scale,maxRadius=150/scale)
+
 
 #display
 if visual:
@@ -57,16 +62,18 @@ if visual:
         print("circles was null or something...")
     else:
         circles = np.uint16(np.around(circles))
+        height, width, depth = displayImage.shape
+        
         for i in circles[0,:]:
             # draw the outer circle
-            cv2.circle(displayImage,(i[0],i[1]),i[2],(0,255,0),2)
+            cv2.circle(displayImage,(scale * i[0], scale * i[1]), scale * i[2], (0, 255, 0), 2)
             # draw the center of the circle
-            cv2.circle(displayImage,(i[0],i[1]),2,(0,0,255),3)
+            cv2.circle(displayImage,(scale * i[0], scale * i[1]), 2, (0, 0, 255), 3)
             #print(str(i[2]))
 
     cv2.imshow('source image', displayImage)
     cv2.imshow('gray image', imgGray)
     cv2.moveWindow('source image', 0, 25)
-    cv2.moveWindow('gray image', 650, 25)
+    cv2.moveWindow('gray image', width + 10, 25)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
